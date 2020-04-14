85a5819fa0dec218e0e3ee893cbdeb7a6622c797
raydangaspar <raydangaspar@gmail.com>
2020-04-14 17:23:48 -0300

    printando usuários de um lance

--- dominio.py
+++ dominio.py
@@ -3,6 +3,24 @@ class Usuario:
     def __init__(self, nome):
         self.__nome = nome
 
-    @property
+    @property   # para acessar o atributo privado
     def nome(self):
         return self.__nome
+
+
+class Lance:
+
+    def __init__(self, usuario, valor):
+        self.usuario = usuario
+        self.valor = valor
+
+
+class Leilao:
+
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.__lances = []
+
+    @property   # para acessar o atributo privado
+    def lances(self):
+        return self.__lances
