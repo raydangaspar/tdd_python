01f10fd96e3b2fa6967f55cee215b78dbc39caf7
raydangaspar <raydangaspar@gmail.com>
2020-04-19 10:21:16 -0300

    refatorando

--- dominio.py
+++ dominio.py
@@ -1,6 +1,3 @@
-import sys
-
-
 # primeira regra de negócio: um usuário não pode dar dois lances seguidos
 # segunda regra de negócio: não posso dar um lance menor ou igual que o lance anterior
 class Usuario:
@@ -10,7 +7,7 @@ class Usuario:
         self.__carteira = carteira
 
     def propoe_lance(self, leilao, valor):
-        if valor > self.__carteira:
+        if not self._valor_eh_valido(valor):
             raise ValueError('Não pode propor um lance com valor maior que  valor da carteira')
 
         lance = Lance(self, valor)
@@ -24,6 +21,9 @@ class Usuario:
     @property
     def carteira(self):
         return self.__carteira
+
+    def _valor_eh_valido(self, valor):
+        return valor <= self.__carteira
 
 
 class Lance:
@@ -40,9 +40,9 @@ class Leilao:
         self.__lances = []
         # self.__lances = set()
         # colocar um valor muito baixo, a ponto de qualquer lance ser maior que ele
-        self.maior_lance = sys.float_info.min  # o menor valor float que há no sistema
+        self.maior_lance = 0.0
         # colocar um valor muito alto, a ponto de qualquer lance ser menor que ele
-        self.menor_lance = sys.float_info.max  # o maior valor float que o sistema pode ter
+        self.menor_lance = 0.0
 
     # reduz acoplamento
     def propoe(self, lance: Lance):  # diga, não pergunte
@@ -50,11 +50,11 @@ class Leilao:
         # if len(self.__lances) == 0 or self.lances[-1].usuario != lance.usuario:
         # self.__lances retorna true se tiver algum item dentro
         # um lance só pode ser aprovado se for maior que o lance anterior
-        if not self.__lances or self.lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:  # jeito paythonico
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
+        if self._lance_eh_valido(lance):  # jeito paythonico
+            if not self._tem_lances():
                 self.menor_lance = lance.valor
+            self.maior_lance = lance.valor
+
             self.__lances.append(lance)
             # self.__lances.add(lance)
         else:
@@ -65,3 +65,16 @@ class Leilao:
         # return self.__lances    # estamos devolvendo o mesmo endereço de memória da lista criada
         # vamos devolver uma cópia dessa lista
         return self.__lances[:]  # cópia rasa de lista
+
+    def _tem_lances(self):
+        return self.__lances
+
+    def _usuarios_diferentes(self, lance):
+        return self.__lances[-1].usuario != lance.usuario
+
+    def _valor_maior_que_lance_anterior(self, lance):
+        return lance.valor > self.__lances[-1].valor
+
+    def _lance_eh_valido(self, lance):
+        return not self._tem_lances() or (self._usuarios_diferentes(lance) and
+                                          self._valor_maior_que_lance_anterior(lance))
